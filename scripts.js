6abce6e19644bd0039c0f657cbc9c1e1d6a0f097
Rodrigo de Macêdo Ferreira <wrodrigo.macedo@gmail.com>
2019-05-02 08:48:05 -0300

    Atualizado o script de comparação de datas; Atualizado o esquema de cores para atrasado e do dia

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 jQuery(function(){
     //alert(11);
     setInterval(highlight, 2000);
+    //setTimeout(highlight, 2000);
 });
 
 const STYLE_BG_INITIAL = "background-color: initial";
-const STYLE_BG_HIGH_PRIORITY = "color: #F00 !important";
-const STYLE_BG_LATE = "background-color: #fcf2ba !important; color: #F00";
-const STYLE_BG_READY2DEPLOY = "background-color: #bbf9a9 !important";
+const STYLE_BG_PRIORITY_HIGH = "color: #F00 !important";
+
+const STYLE_BG_DATE_LATE = "background-color: #ffbaba !important; color: #F00";
+const STYLE_BG_DATE_TODAY = "background-color: #fcf2ba !important; color: #847500";
+
+const STYLE_BG_STATUS_READY2DEPLOY = "background-color: #bbf9a9 !important";
 
 
 
@@ -36,6 +40,7 @@ function processRow(){
         paintRow(row, STYLE_BG_INITIAL);
         highlightPriority(row);
         highlightSituation(row);
+        highlightTodayTasks(row);
         highlightLateTasks(row);
     });
     
@@ -43,44 +48,62 @@ function processRow(){
 
 
 function highlightPriority(row){
-    let isHighPriority = (txt) => {
+    let willPaintRow = (txt) => {
         return txt.toLowerCase().indexOf('alta') !== -1;
     }
     let col = row.find("span.wp-table--cell-container.priority");
     let strValueColumn = row.find("span.wp-table--cell-container.priority").text();
     
-    if(isHighPriority(strValueColumn)){
-        paintRow(col.parent(), STYLE_BG_HIGH_PRIORITY);
+    if(willPaintRow(strValueColumn)){
+        paintRow(col.parent(), STYLE_BG_PRIORITY_HIGH);
     }
 }
 
 
 function highlightSituation(row){
-    let isReadyToDeploy = (txt) => {
+    let willPaintRow = (txt) => {
         return txt.toLowerCase().indexOf('pronto para deploy') !== -1;
     }
     let col = row.find("span.wp-table--cell-container.status");
     let strValueColumn = col.text();
     
-    if(isReadyToDeploy(strValueColumn)){
-        paintRow(col.parent(), STYLE_BG_READY2DEPLOY);
+    if(willPaintRow(strValueColumn)){
+        paintRow(col.parent(), STYLE_BG_STATUS_READY2DEPLOY);
     }
 }
 
 
-function highlightLateTasks(row){
-    let isReadyToDeploy = (txt) => {
-        var parts = txt.split("/");
-        var dt = new Date(parseInt(parts[2], 10),
-                  parseInt(parts[1], 10) - 1,
-                  parseInt(parts[0], 10));
-        return dt < (new Date);
+function highlightTodayTasks(row){
+    let willPaintRow = (txt) => {
+        let date = parseInt(txt.split('/').reverse().join(''));
+        let today = parseInt((new Date).toLocaleString().split(" ")[0].split('/').reverse().join(''))
+        //console.log('_rdmf_ highlightLateTasks dt: ', dt, (new Date), dt < (new Date));
+        return date == today;
     }
     
     let col = row.find("span.wp-table--cell-container.dueDate");
     let strValueColumn = col.text();
     
-    if(isReadyToDeploy(strValueColumn)){
-        paintRow(col.parent(), STYLE_BG_LATE);
+    if(willPaintRow(strValueColumn)){
+        //console.log(strValueColumn + " highlightTodayTasks");
+        paintRow(col.parent(), STYLE_BG_DATE_TODAY);
+    }
+}
+
+
+function highlightLateTasks(row){
+    let willPaintRow = (txt) => {
+        let date = parseInt(txt.split('/').reverse().join(''));
+        let today = parseInt((new Date).toLocaleString().split(" ")[0].split('/').reverse().join(''))
+        //console.log('_rdmf_ highlightLateTasks dt: ', dt, (new Date), dt < (new Date));
+        return date < today;
+    }
+    
+    let col = row.find("span.wp-table--cell-container.dueDate");
+    let strValueColumn = col.text();
+    
+    if(willPaintRow(strValueColumn)){
+        //console.log(strValueColumn + " highlightLateTasks");
+        paintRow(col.parent(), STYLE_BG_DATE_LATE);
     }
 }
